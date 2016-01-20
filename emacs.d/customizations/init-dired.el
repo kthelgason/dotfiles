6570dd7362383118cf12602b4ea7aae452f822fe
Kári Tristan Helgason <kthelgason@gmail.com>
2016-01-20 19:25:53 +0000

    remove guide-key from dired config

--- emacs.d/customizations/init-dired.el
+++ emacs.d/customizations/init-dired.el
@@ -14,9 +14,7 @@
   (when (fboundp 'global-dired-hide-details-mode)
     (global-dired-hide-details-mode -1))
   (setq dired-recursive-deletes 'top)
-  (define-key dired-mode-map [mouse-2] 'dired-find-file)
-  (add-hook 'dired-mode-hook
-            (lambda () (guide-key/add-local-guide-key-sequence "%"))))
+  (define-key dired-mode-map [mouse-2] 'dired-find-file))
 
 (when (maybe-require-package 'diff-hl)
   (after-load 'dired
