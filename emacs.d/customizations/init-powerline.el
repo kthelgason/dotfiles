9da5bbc11f52129ab98252bf4a48d6ad9197b220
Kári Tristan Helgason <kthelgason@gmail.com>
2016-04-19 09:49:43 +0000

    modeline customizations

--- emacs.d/customizations/init-powerline.el
+++ emacs.d/customizations/init-powerline.el
@@ -1,60 +1,10 @@
 (require-package 'powerline)
 (require-package 'powerline-evil)
-(require 'powerline)
-(require 'powerline-evil)
 
-(defun kthelgason/powerline-theme ()
-  "Powerline's Vim-like mode-line with evil state at the beginning in color."
-  (interactive)
-  (setq-default mode-line-format
-                '("%e"
-                  (:eval
-                   (let* ((active (powerline-selected-window-active))
-                          (mode-line (if active 'mode-line 'mode-line-inactive))
-                          (face1 (if active 'powerline-active1 'powerline-inactive1))
-                          (face2 (if active 'powerline-active2 'powerline-inactive2))
-                          (separator-left (intern (format "powerline-%s-%s"
-                                                          (powerline-current-separator)
-                                                          (car powerline-default-separator-dir))))
-                          (separator-right (intern (format "powerline-%s-%s"
-                                                           (powerline-current-separator)
-                                                           (cdr powerline-default-separator-dir))))
-                          (lhs (list (let ((evil-face (powerline-evil-face)))
-                                       (if evil-mode
-                                           (powerline-raw (powerline-evil-tag) evil-face)))
-                                     (powerline-buffer-id `(mode-line-buffer-id ,mode-line) 'l)
-                                     (funcall separator-left mode-line face1)
-                                     (powerline-major-mode mode-line)
-                                     (funcall separator-left mode-line face1)
-                                     (when (buffer-modified-p)
-                                       (powerline-raw "[+]" mode-line))
-                                     (when buffer-read-only
-                                       (powerline-raw "[RO]" mode-line))
-                                     (powerline-raw "[%z]" mode-line)
-                                     ;; (powerline-raw (concat "[" (mode-line-eol-desc) "]") mode-line)
-                                     (when (and (boundp 'which-func-mode) which-func-mode)
-                                       (powerline-raw which-func-format nil 'l))
-                                     (when (boundp 'erc-modified-channels-object)
-                                       (powerline-raw erc-modified-channels-object face1 'l))
-                                     (powerline-raw "[" mode-line 'l)
-                                     (powerline-minor-modes mode-line)
-                                     (powerline-raw "%n" mode-line)
-                                     (powerline-raw "]" mode-line)
-                                     (when (and vc-mode buffer-file-name)
-                                       (let ((backend (vc-backend buffer-file-name)))
-                                         (when backend
-                                           (concat (powerline-raw "[" mode-line 'l)
-                                                   (powerline-raw (format "%s / %s" backend (vc-working-revision buffer-file-name backend)))
-                                                   (powerline-raw "]" mode-line)))))))
-                          (rhs (list (powerline-raw '(10 "%i"))
-                                     (powerline-raw global-mode-string mode-line 'r)
-                                     (powerline-raw "%l," mode-line 'l)
-                                     (powerline-raw (format-mode-line '(10 "%c")))
-                                     (powerline-raw (replace-regexp-in-string  "%" "%%" (format-mode-line '(-3 "%p"))) mode-line 'r))))
-                     (concat (powerline-render lhs)
-                             (powerline-fill mode-line (powerline-width rhs))
-                             (powerline-render rhs)))))))
+(require 'spaceline-config)
 
-(powerline-evil-vim-color-theme)
+(spaceline-spacemacs-theme)
+(setq spaceline-highlight-face-func
+      'spaceline-highlight-face-evil-state)
 
 (provide 'init-powerline)
